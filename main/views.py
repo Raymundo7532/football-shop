49ea6ea86f193718723a074e1f7af0a9175ded75
Raymundo7532 <woloblocrafael@gmail.com>
2025-09-21 20:40:12 +0700

    Membatasi Akses Halaman Main dan Product Detail

--- main/views.py
+++ main/views.py
@@ -1,12 +1,16 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+import datetime
+from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core import serializers
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from main.forms import ProductForm
-from django.http import HttpResponse
-from django.core import serializers
 from main.models import Product
 
+@login_required(login_url='/login')
 def show_main(request):
     product_list = Product.objects.all()
 
@@ -15,7 +19,8 @@ def show_main(request):
         'name': 'Raymundo Rafaelito Maryos Von Woloblo',
         'class': 'PBP B',
         'app_name' : 'main',
-        'product_list' : product_list
+        'product_list' : product_list,
+        'last_login': request.COOKIES.get('last_login', 'Never'),
     }
 
     return render(request, "main.html", context)
@@ -30,6 +35,7 @@ def create_product(request):
     context = {'form': form}
     return render(request, "create_product.html", context)
 
+@login_required(login_url='/login')
 def show_product(request, id):
     product = get_object_or_404(Product, pk=id)
     product.increment_views()
@@ -79,19 +85,23 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+        if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('main:show_main')
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
 
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
-    return redirect('main:login')
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
